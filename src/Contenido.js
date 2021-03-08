33a6010f76571704598b84b99dd2710395d6bdc4
Aaron Trujillo <atrujilloembry@gmail.com>
2021-03-07 21:28:15 -0300

    contenido contador

---
+++ src/Contenido.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+const Contenido = () => {
+  const [segundos, setsegundos] = useState(0);
+
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setsegundos(segundos => segundos + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+		return(
+			<div className="container">
+        <h1> Contenido </h1>
+        <div className="contenido">
+        <i className="far fa-clock"></i>
+        <p className="informacion">
+        {segundos}
+        </p>
+        </div>
+      </div>      
+			);
+
+	}
+
+export default Contenido;
